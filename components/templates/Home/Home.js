08031765d7ad77ab88fe0839756fb46edde5f4fd
Alex Wade <alexbwade@gmail.com>
2022-12-22 15:40:21 -0800

    initial - copy old site

--- components/templates/Home/Home.js
+++ components/templates/Home/Home.js
@@ -1,3 +1,44 @@
+import styles from "./Home.module.scss";
+
 export default function Home() {
-  return <main>Hi</main>;
+  return (
+    <div className={styles.wrapper}>
+      <aside className={styles.portrait}>
+        <div className={styles.photoBy}>
+          Photo by
+          <a href="https://www.badtactic.com/" target="_blank" rel="noreferrer">
+            Matthew Thogerson
+          </a>
+        </div>
+      </aside>
+      <main className={styles.content}>
+        <div>
+          <h1>
+            Hi there, I'm <span>Alex</span>!
+          </h1>
+          <h2>
+            I am a front-end focused <strong>software engineer</strong> based in the Portland area.
+          </h2>
+          <p>
+            My passion is building fast, user-focused web applications that feel intuitive and add value to people's
+            lives.
+          </p>
+          <p>
+            <em>
+              (Currently, I am working on the Filter Cloud team at{" "}
+              <a
+                className={styles.textLightspeed}
+                href="https://www.lightspeedsystems.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Lightspeed Systems
+              </a>
+              .)
+            </em>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
