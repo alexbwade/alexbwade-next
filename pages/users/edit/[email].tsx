3f41c7f46fd37b8f98f0c5b75678c9ff3572d700
Alex Wade <alexbwade@gmail.com>
2025-05-10 09:26:58 -0700

    chore: misc next fixes

--- pages/users/edit/[email].tsx
+++ pages/users/edit/[email].tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Mulish } from "@next/font/google";
+import { Mulish } from "next/font/google";
 
 import { EditUser as EditUserComponent } from "~templates";
 
