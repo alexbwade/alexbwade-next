144483192a5eca3293e2cac55f2fd99b63cdcd2d
Alex Wade <alexbwade@gmail.com>
2026-01-28 14:30:23 -0800

    maybe fix

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    root: __dirname,
+  },
   compiler: {
     reactRemoveProperties: {
       properties: ["^data-testid$"],
     },
-    // removeConsole: {
-    //   exclude: ["error"],
-    // },
   },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.edgeql/,
-  //     use: "raw-loader",
-  //   });
-
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
